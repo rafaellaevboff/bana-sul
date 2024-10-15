e753748223301278f17ac854c36815edf62aae71
rafaellaevboff <rafaella-eboff@rede.ulbra.br>
2024-10-15 00:54:13 -0300

    Altera variáveis do Firebase para primeiro deploy no Vercel

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -1,8 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
-import firebaseConfig from './firebaseConfig';
 import {getFirestore} from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+    authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.VUE_APP_FIREBASE_APP_ID,
+};
 
 const app = initializeApp(firebaseConfig);
 
