f95d6fde4cb980fff975f68894f4413459060fed
rafaellaevboff <rafaella-eboff@rede.ulbra.br>
2024-11-19 22:32:41 -0300

    Melhora design de tela not-found

--- src/pages/NotFoundScreen.vue
+++ src/pages/NotFoundScreen.vue
@@ -1,22 +1,11 @@
 <template>
-  <v-container class="fill-height">
-    <v-row
-            class="d-flex align-center justify-center text-center fill-height"
-    >
-      <v-col cols="12">
+  <v-container fluid>
+    <v-row class="center" >
+      <v-col cols="12" align-self="center">
         <v-icon size="128" color="primary">mdi-emoticon-sad-outline</v-icon>
         <h1 class="display-1">404</h1>
-        <p class="subtitle-1">
-          Oops! A página que você está procurando não existe.
-        </p>
-        <v-btn
-                color="primary"
-                variant="outlined"
-                size="large"
-                to="/"
-        >
-          Voltar para a Home
-        </v-btn>
+        <p class="subtitle-1">Ops! A página que você está procurando não existe.</p>
+        <v-btn color="primary" variant="outlined" size="large" to="/">Voltar para a Home</v-btn>
       </v-col>
     </v-row>
   </v-container>
@@ -26,8 +15,16 @@
 import { VContainer, VRow, VCol, VIcon, VBtn } from "vuetify/components";
 </script>
 
-<style scoped>
-.fill-height {
-    height: 100vh; /* Ocupa toda a altura da tela */
+<style>
+html, body {
+    height: 100%;
+    margin: 0;
+}
+
+.center {
+    height: 90vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 }
 </style>
